1018f51130bfdd16b3d6bfb72ee06a7a947d95d0
AsherSun <asher_sun@163.com>
2019-07-16 14:49:31 +0800

    docs(async.js)
    初步注释

--- src/util/async.js
+++ src/util/async.js
@@ -1,6 +1,11 @@
 /* @flow */
 
+/*
+* @params: queue 为导航守卫队列
+**/
 export function runQueue (queue: Array<?NavigationGuard>, fn: Function, cb: Function) {
+  // step 方法为一个递归循环方法。
+  // 递归结束条件：index >= queue.length
   const step = index => {
     if (index >= queue.length) {
       cb()
