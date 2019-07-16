c84a0e16abf902debe244474eae90f9ee355fff7
AsherSun <asher_sun@163.com>
2019-07-16 15:17:26 +0800

    docs(params.js)
    代码初步注释

--- src/util/params.js
+++ src/util/params.js
@@ -4,17 +4,23 @@ import { warn } from './warn'
 import Regexp from 'path-to-regexp'
 
 // $flow-disable-line
+// 创建一个regexpCompileCache对象，其对象下面的key都为function
 const regexpCompileCache: {
   [key: string]: Function
 } = Object.create(null)
 
+// 参数处理
 export function fillParams (
-  path: string,
-  params: ?Object,
+  path: string, // 路径
+  params: ?Object, // 可选参数
   routeMsg: string
 ): string {
+  // 因为 params 是可选参数，所以需要坐下兼容处理
   params = params || {}
   try {
+    // 该语句可以解构为这样：
+    // const filler =  regexpCompileCache[path] ?  regexpCompileCache[path] : (regexpCompileCache[path] = Regexp.compile(path))
+    // 存在则使用，不存在则创建
     const filler =
       regexpCompileCache[path] ||
       (regexpCompileCache[path] = Regexp.compile(path))
