fa381084d03adc8b2fc436fd2809cb3888418e7c
AsherSun <asher_sun@163.com>
2019-07-16 15:52:04 +0800

    docs(path.js)
    添加代码注释

--- src/util/path.js
+++ src/util/path.js
@@ -1,26 +1,29 @@
 /* @flow */
 
+// 返回路径
 export function resolvePath (
   relative: string,
   base: string,
   append?: boolean
 ): string {
+  // 拿到第一个字符串
   const firstChar = relative.charAt(0)
-  if (firstChar === '/') {
+  if (firstChar === '/') { // 判断是否存在“根路径”, 存在则返回
     return relative
   }
 
-  if (firstChar === '?' || firstChar === '#') {
+  if (firstChar === '?' || firstChar === '#') {// hash 模式的路径拼接
     return base + relative
   }
-
+  
+  // 获取路径上的所有路由 path 可以理解为堆栈数据结构
   const stack = base.split('/')
 
   // remove trailing segment if:
   // - not appending
   // - appending to trailing slash (last segment is empty)
-  if (!append || !stack[stack.length - 1]) {
-    stack.pop()
+  if (!append || !stack[stack.length - 1]) { // 如果不是前进，并且栈内还有数据
+    stack.pop() // 出栈，栈尾删除
   }
 
   // resolve relative path
@@ -28,20 +31,22 @@ export function resolvePath (
   for (let i = 0; i < segments.length; i++) {
     const segment = segments[i]
     if (segment === '..') {
-      stack.pop()
+      stack.pop() // 出栈，栈尾删除
     } else if (segment !== '.') {
-      stack.push(segment)
+      stack.push(segment) // 进栈，栈尾添加
     }
   }
 
   // ensure leading slash
-  if (stack[0] !== '') {
-    stack.unshift('')
+  // 确保后面的 stack.join('/') 第一个字符为 '/'
+  if (stack[0] !== '') { // 如果不是空字符串
+    stack.unshift('') // 进栈，栈顶添加
   }
 
-  return stack.join('/')
+  return stack.join('/') // 将数组解析为一个字符串并且返回
 }
 
+// 路径解析
 export function parsePath (path: string): {
   path: string;
   query: string;
@@ -69,6 +74,7 @@ export function parsePath (path: string): {
   }
 }
 
+// 清除路径
 export function cleanPath (path: string): string {
   return path.replace(/\/\//g, '/')
 }
